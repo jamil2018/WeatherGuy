6e317b7eb1b6ad7cd395ab62dc9466f2030b0908
Hasnat Jamil <hasnatjamil2018@gmail.com>
2020-11-20 13:31:19 +0600

    changes: 1. change project structure 2. add project images

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Typography, CssBaseline } from "@material-ui/core";
+
+function App() {
+  return (
+    <>
+      <CssBaseline />
+      <Typography variant="h1">Hello World</Typography>
+    </>
+  );
+}
+export default App;
