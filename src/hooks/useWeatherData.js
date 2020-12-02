be651a36302dbdd39d56aa06828d67e6bad83b58
Hasnat Jamil <hasnatjamil2018@gmail.com>
2020-12-02 22:37:41 +0600

    fix weather api request error

--- src/hooks/useWeatherData.js
+++ src/hooks/useWeatherData.js
@@ -11,7 +11,7 @@ const useWeatherData = (lon, lat) => {
         try {
           const cancelToken = axios.CancelToken;
           const source = cancelToken.source();
-          const reqURI = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_API_KEY}`;
+          const reqURI = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_API_KEY}`;
           const weatherData = await axios.get(reqURI);
           setRes(weatherData);
           return source.cancel("axios request cancelled");
